7ed7cbcf17ac3d3ae8a8e006e423155e7e273711
Cody <mizx@users.noreply.github.com>
2018-07-17 22:14:16 -0700

    added mui CssBaseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import * as React from 'react';
-import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import React from 'react';
 
-import logo from './logo.svg';
-
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App: React.SFC = () => (
+  <>
+    <CssBaseline />
+    <h1>hello world</h1>
+  </>
+)
 
 export default App;
